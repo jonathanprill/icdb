3f6ef1c08e3cf4a7e1a764b2cdc9e12b8618905f
jonathanprill <jonathantprill@gmail.com>
2022-06-30 11:32:25 -0500

    adds responsive side nav bar

---
+++ src/pages/Products.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Products() {
+  return (
+    <div style={{paddingLeft: '400px'}}>
+      <h1>Products</h1>
+      <h1>Products</h1>
+      <h1>Products</h1>
+      <h1>Products</h1>
+      <h1>Products</h1>
+      <h1>Products</h1>
+      <h1>Products</h1>
+      <h1>Products</h1>
+    </div>
+  );
+}
+
+export default Products;
---
+++ src/pages/Reports.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Reports() {
+  return (
+    <div style={{paddingLeft: '400px'}}>
+      <h1>Reports</h1>
+      <h1>Reports</h1>
+      <h1>Reports</h1>
+      <h1>Reports</h1>
+      <h1>Reports</h1>
+      <h1>Reports</h1>
+      <h1>Reports</h1>
+      <h1>Reports</h1>
+    </div>
+  );
+}
+
+export default Reports;
